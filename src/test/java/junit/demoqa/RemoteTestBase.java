eafda82c83e31e2f32a7a4571c6402d9ba106635
Anton Malov <maloanto@yandex.ru>
2023-07-09 19:22:35 +0300

    added comment

--- src/test/java/junit/demoqa/RemoteTestBase.java
+++ src/test/java/junit/demoqa/RemoteTestBase.java
@@ -26,7 +26,7 @@ public class RemoteTestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
