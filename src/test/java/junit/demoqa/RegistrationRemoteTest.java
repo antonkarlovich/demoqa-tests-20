cbc843f66576bdab0a7230dc4b1ebab0c99e9881
Anton Malov <maloanto@yandex.ru>
2023-07-09 16:37:21 +0300

    added allure steps

--- src/test/java/junit/demoqa/RegistrationRemoteTest.java
+++ src/test/java/junit/demoqa/RegistrationRemoteTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 
 public class RegistrationRemoteTest extends RemoteTestBase {
 
@@ -12,32 +14,36 @@ public class RegistrationRemoteTest extends RemoteTestBase {
     @DisplayName("Successful registration")
     void successfulRegistrationTest() {
 
-        registrationPage.openPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setEmail(testData.userEmail)
-                .setGender(testData.gender)
-                .setPhoneNumber(testData.phoneNumber)
-                .setBirthDay(testData.dayOfBirth, testData.monthOfBirth, testData.yearOfBirth)
-                .setSubject(testData.subject)
-                .setHobbies(testData.hobby)
-                .uploadFile(testData.pathPng)
-                .setAddress(testData.address)
-                .selectState(testData.state)
-                .selectCity(testData.city)
-                .clickSubmit();
+        step("Открытие и заполнение формы", () -> {
+            registrationPage.openPage()
+                    .setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.userEmail)
+                    .setGender(testData.gender)
+                    .setPhoneNumber(testData.phoneNumber)
+                    .setBirthDay(testData.dayOfBirth, testData.monthOfBirth, testData.yearOfBirth)
+                    .setSubject(testData.subject)
+                    .setHobbies(testData.hobby)
+                    .uploadFile(testData.pathPng)
+                    .setAddress(testData.address)
+                    .selectState(testData.state)
+                    .selectCity(testData.city)
+                    .clickSubmit();
+        });
 
-        registrationPage.checkVisibleTable()
-                .verifyResult("Student Name", testData.firstName + " " + testData.lastName)
-                .verifyResult("Student Email", testData.userEmail)
-                .verifyResult("Gender", testData.gender)
-                .verifyResult("Mobile", testData.phoneNumber)
-                .verifyResult("Date of Birth", testData.dayOfBirth + " " + testData.monthOfBirth +
-                        "," + testData.yearOfBirth)
-                .verifyResult("Subjects", testData.subject)
-                .verifyResult("Hobbies", testData.hobby)
-                .verifyResult("Picture", testData.pathPng)
-                .verifyResult("Address", testData.address)
-                .verifyResult("State and City", testData.state + " " + testData.city);
-   }
+        step("проверка результатов", () -> {
+            registrationPage.checkVisibleTable()
+                    .verifyResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .verifyResult("Student Email", testData.userEmail)
+                    .verifyResult("Gender", testData.gender)
+                    .verifyResult("Mobile", testData.phoneNumber)
+                    .verifyResult("Date of Birth", testData.dayOfBirth + " " + testData.monthOfBirth +
+                            "," + testData.yearOfBirth)
+                    .verifyResult("Subjects", testData.subject)
+                    .verifyResult("Hobbies", testData.hobby)
+                    .verifyResult("Picture", testData.pathPng)
+                    .verifyResult("Address", testData.address)
+                    .verifyResult("State and City", testData.state + " " + testData.city);
+        });
+    }
 }
